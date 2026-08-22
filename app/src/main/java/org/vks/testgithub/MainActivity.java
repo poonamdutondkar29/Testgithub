145afd061b2a6cb91653296d8cb7bb8ca92b85ef
Poonam <poonamdutondkar@gmail.com>
2026-08-22 10:44:21 +0530

    commit

--- app/src/main/java/org/vks/testgithub/MainActivity.java
+++ app/src/main/java/org/vks/testgithub/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         //First commit 333333333333333333
+        //first brand
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
